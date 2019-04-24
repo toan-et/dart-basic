77123983b863b6c17f9f8a0c2aa9624cef833ff0
Phillip <phillip7.et@gmail.com>
2019-04-24 12:12:24 +0700

    ...

--- L6/Avenger.dart
+++ L6/Avenger.dart
@@ -4,21 +4,13 @@ class Avenger {
     this._name = name;
   }
   void showInfo() {
-    print(getInfo());
+    
   }
   String getInfo() => "I'm: " + _name;
 }
 
 class IronMan extends Avenger {
-  IronMan({name}) : super(name:name);
-  @override
-  void showInfo() {
-    print(this.getInfo());    
-  }
-   @override
-  String getInfo() {
-    return super.getInfo();
-  }
+ 
 }
 void main() {
   Avenger av = new Avenger();
