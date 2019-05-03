54ff9d62dea8a28dfb47fd1200e432e3d7f1b142
Phillip <phillip7.et@gmail.com>
2019-05-03 16:54:37 +0700

    final L5

--- L5/Point.dart
+++ L5/Point.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 class Point {
   int _x;
   int _y;
@@ -13,7 +14,26 @@ class Point {
     _x = x;
     _y = y;
   }
-  void show(){
-    print('x=$_x, y=$_y');
+
+  void move(Point p) {
+    setXY(x: p.getX(), y: p.getY());
+  }
+
+  void show([String label = 'Point']) {
+    print('$label: x=$_x, y=$_y');
+  }
+
+  static Point plus(Point p1, Point p2) {
+    return Point(x: p1.getX() + p2.getX(), y: p1.getY() + p2.getY());
+  }
+
+  Point operator +(Point p) {
+    return Point(x: _x + p.getX(), y: _y + p.getY());
+  }
+
+  static double calcDistance(Point p1, Point p2) {
+    double d = math.sqrt(math.pow((p2.getX() - p1.getX()), 2) +
+        math.pow((p2.getY() - p1.getY()), 2));
+    return d;
   }
 }
