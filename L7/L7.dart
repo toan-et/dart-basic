81c9875f5fb857748f11e65f99154cc930d7c106
Phillip <phillip7.et@gmail.com>
2019-05-24 18:04:33 +0700

    final code

---
+++ L7/L7.dart
@@ -0,0 +1,17 @@
+import '../L6/CaptainAmerica.dart';
+import '../L6/Thanos.dart';
+import '../L6/Hulk.dart';
+import '../L6/Spiderman.dart';
+import 'dart:math';
+List<dynamic> avengers = [Thanos(), Hulk(), Spiderman(), CaptainAmerica()];
+String checkAvenger(dynamic avenger){
+  return avenger.runtimeType.toString();
+}
+dynamic getAvenger(){
+  var random = new Random();
+  return avengers[random.nextInt(4)];
+}
+void main(){
+  for(var i = 0; i<20; i++)
+    print(checkAvenger(getAvenger()));
+}
