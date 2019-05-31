f943a28b799b996287b1d57db2b6d4cb5d81e9ab
Phillip <phillip7.et@gmail.com>
2019-05-31 17:54:23 +0700

    Update L8 code
    Add more Avenger

--- L8/L8.1.dart
+++ L8/L8.1.dart
@@ -8,123 +8,114 @@ import '../L6/CaptainAmerica.dart';
 import '../L6/Thanos.dart';
 import '../L6/Hulk.dart';
 import '../L6/Thor.dart';
+
 const STREAM_CLOSE_TIMEOUT = 4;
 const AVENGER_GO_TIMEOUT = 1;
 AnsiPen greenText = AnsiPen()..green(bold: true);
 AnsiPen redText = AnsiPen()..red(bold: true);
-AnsiPen yellowText = AnsiPen()..yellow(bold: true);    
+AnsiPen yellowText = AnsiPen()..yellow(bold: true);
 void onCloseHandler() {
-  print('⌚  Stream closed after ${STREAM_CLOSE_TIMEOUT}s');
+  print('[StreamController.close()] ---> ⌚  ' +
+      greenText("${STREAM_CLOSE_TIMEOUT}s passed. Stream is closed"));
 }
-bool isReadyGadget(Avenger avenger){
+
+bool isReadyGadget(Avenger avenger) {
   return avenger.hasGadget();
 }
-Future delay(int seconds){
-  // var timer = TimeDisplay();
-  // Console.write("⌚  ");
-  // timer.start();
-  // return Future.delayed(Duration(seconds: seconds)).then((_) {
-  //   timer.stop();
-  // });
-  return new Future.delayed(Duration(seconds: seconds), () => print('⌚  ${seconds.toString()}s'));
+
+Future delay(int seconds) {
+  return new Future.delayed(
+      Duration(seconds: seconds), () => print('⌚  ${seconds.toString()}s'));
 }
-void addAvengers(StreamController streamCtrl, List<dynamic> avengers) async{
-  for(final avenger in avengers){
-      try {        
-        print('${yellowText('>>>')} Pushed ' + avenger.runtimeType.toString());
-        await streamCtrl.sink.add(avenger);
-        await delay(AVENGER_GO_TIMEOUT);
-      } catch (e) {
-        if(streamCtrl.isClosed){
-          print('${redText('☒ Stream is closed ☒')}');
-          print('${redText('✖')} ${redText(avenger.runtimeType.toString())} is late ');
-        }
-        else{
-          print(e);
-        }       
-        break;
-      }
+
+void addAvengers(StreamController streamCtrl, List<dynamic> avengers) async {
+  for (final avenger in avengers) {
+    try {
+      print('${yellowText('>>>')} Pushed ' + avenger.runtimeType.toString());
+      if (streamCtrl.isClosed) {
+        print('${redText('☒ Stream is closed ☒')}');
+        print(
+            '${redText('✖')} ${redText(avenger.name)} is late ');
+      } else
+        streamCtrl.sink.add(avenger);
+      await delay(AVENGER_GO_TIMEOUT);
+    } catch (e) {
+      print(e);
+    }
+    // break;
   }
-  // for(var i = 0; i<avengers.length; i++){
-  //   try {
-  //     print('>>> Pushed ' + avengers[i].runtimeType.toString());
-  //     await streamCtrl.sink.add(avengers[i]);
-  //     await delay(AVENGER_GO_TIMEOUT);
-  //   } catch (e) {
-  //     print('''
-  //     ☒ Stream is closed ☒
-  //     ✗ ${avengers[i].runtimeType.toString()} is late ✗
-  //     ''');
-  //     i = avengers.length;
-  //   }
-  // }
 }
+
 Future<List<dynamic>> transformAvengers(List<dynamic> avengers) async {
   List<dynamic> transformedAvengers = [];
 
   StreamController streamCtrl = new StreamController(
-        onCancel: onCloseHandler,
+    onCancel: onCloseHandler,
   );
   StreamSubscription streamSubscription;
-  streamSubscription = streamCtrl.stream.listen((avenger) {         
-        if(isReadyGadget(avenger)) {
-          transformedAvengers.add(avenger);
-          print("${greenText('✓')} Accepted: " + avenger.runtimeType.toString());
-        }
-        else
-          print("${redText('✖')} Rejected: " + avenger.runtimeType.toString() + " -> without gadget");
+  streamSubscription = streamCtrl.stream.listen((avenger) {
+    if (isReadyGadget(avenger)) {
+      transformedAvengers.add(avenger);
+      print("${greenText('✓')} Accepted: " + avenger.runtimeType.toString());
+    } else
+      print("${redText('✖')} Rejected: " +
+          avenger.runtimeType.toString() +
+          " -> without gadget");
 
-      // Add 5 seconds delay
-      // It will call onPause function passed on StreamController constructor
-      //streamSubscription.pause(Future.delayed(const Duration(seconds: 20)));
-    }, onDone: () {
-      print("====> Transforming Avengers done");
-    }, onError: (error) {
-      print("Some Error");
-    }
-  );
-  Future.delayed(const Duration(seconds: STREAM_CLOSE_TIMEOUT),(){
+    // Add 5 seconds delay
+    // It will call onPause function passed on StreamController constructor
+    //streamSubscription.pause(Future.delayed(const Duration(seconds: 20)));
+  }, onDone: () {
+    print("[StreamSubscription.onDone()] ---> " +
+        greenText('Transforming Avengers done'));
+  }, onError: (error) {
+    print("Some Error");
+  });
+  Future.delayed(const Duration(seconds: STREAM_CLOSE_TIMEOUT), () {
     streamCtrl.close();
-  }); 
+  });
   await addAvengers(streamCtrl, avengers);
   return transformedAvengers;
 }
 
 // Liệt kê avengers đã qua đường hầm lượng tử
-void showAvengers(List<dynamic> transformedAvengers){
-  print('\r\n<====Final Transformed Avenger=====>');
-  transformedAvengers.forEach((avenger){
+void showAvengers(List<dynamic> transformedAvengers) {
+  print('\r\n<========[Final Transformed Avengers]========>');
+  transformedAvengers.forEach((avenger) {
     print(avenger.runtimeType.toString());
   });
 }
+
 List<dynamic> avengers = [
   Thanos(
-    name:'Củ khoai tím',
-    sexual:'Male',
-    gadget:Gadget('Găng tay vô cực 6 đá')
+    name: 'Củ khoai tím',
+    sexual: 'Male',
+    gadget: Gadget('Găng tay vô cực 6 đá'),
   ),
-  Thor(
-    name:'Thần Sét',
-    sexual:'Male'
-  ),
+  Thor(name: 'Thần Sét', sexual: 'Male'),
   CaptainAmerica(
-    name:'Lớp trưởng Mỹ',
-    sexual:'Male',
-    gadget:Gadget('Supper Khiên')
+    name: 'Lớp trưởng Mỹ',
+    sexual: 'Male',
+    gadget: Gadget('Supper Khiên'),
   ),
   Spiderman(
-    name:'Người Nhện',
-    sexual:'Male',
-    gadget:Gadget('Tơ Nhện')
+    name: 'Người Nhện',
+    sexual: 'Male',
+    gadget: Gadget('Tơ Nhện'),
   ),
   Hulk(
-    name:'Khổng Lồ Xanh',
-    sexual:'Male',
-    gadget:Gadget('Cú đấm')
+    name: 'Khổng Lồ Xanh',
+    sexual: 'Male',
+    gadget: Gadget('Cú đấm'),
+  ),
+  Hulk(
+    name: 'Hulk Fake',
+    sexual: 'Male',
+    gadget: Gadget('Cú đấm'),
   ),
 ];
 void main() {
-  transformAvengers(avengers).then((value) { 
+  transformAvengers(avengers).then((value) {
     showAvengers(value);
-  });  
+  });
 }
