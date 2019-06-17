abe097d06277494839b9a7d0efcb4dc9d672912d
Phillip <phillip7.et@gmail.com>
2019-06-17 21:09:02 +0700

    update L11

--- L11/L11.dart
+++ L11/L11.dart
@@ -1,17 +1,19 @@
+// import file here
+
+const List<String> urlAvengers = [
+  'https://blogspotscraping.herokuapp.com/avengers/Thor.json',
+  'https://blogspotscraping.herokuapp.com/avengers/Thanos.json',
+  'https://blogspotscraping.herokuapp.com/avengers/CaptainAmerica.json',
+];
+
+//funtion createAvengers(){
+  // todo
+  // forced call default factory constructor
+//}
+
 void main(){
-  const List<String> urlAvengers = [
-      'https://blogspotscraping.herokuapp.com/avengers/Thor.json',
-      'https://blogspotscraping.herokuapp.com/avengers/Thanos.json',
-      'https://blogspotscraping.herokuapp.com/avengers/CaptainAmerica.json',
-    ];
-
-    //funtion createAvengers(){
-      // todo
-      // forced call default factory constructor
-    //}
-
-    print('============ L11.2 - Call Default Factory Constructor Orderly ============');
-    // todo
-    print('============ L11.1 - Call Named factory constructor create any avenger ============');
-    // todo
+  print('============ L11.2 - Call Default Factory Constructor Orderly ============');
+  // todo
+  print('============ L11.1 - Call Named factory constructor create any avenger ============');
+  // todo
 }
